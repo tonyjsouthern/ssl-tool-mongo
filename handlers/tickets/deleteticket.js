1683ee9e441dbd3682fc2afc47f3d26e15d39ed2
TJ <tonyjsouthern@gmail.com>
2018-10-18 08:20:12 -0700

    fixed res.send error messages on ticket routes

--- handlers/tickets/deleteticket.js
+++ handlers/tickets/deleteticket.js
@@ -2,7 +2,7 @@ function deleteTicket (req, res, next) {
   var id = req.params.id
   req.db.collection(req.collection).deleteOne({_id: new req.object_id(req.params.id)}, function(err, result) {
     if (err) {
-      handleError(res, err.message, "Failed to delete contact");
+      handleError(res, err.message, "Failed to delete ticket");
     } else {
       res.send("Item deleted: " + id)
       res.status(204).end();
--- handlers/tickets/getsingleticket.js
+++ handlers/tickets/getsingleticket.js
@@ -1,7 +1,7 @@
 function getSingleTicket (req, res, next) {
   req.db.collection(req.collection).findOne({ _id: new req.object_id(req.params.id) }, function(err, doc) {
     if (err) {
-      handleError(res, err.message, "Failed to get contact");
+      handleError(res, err.message, "Failed to get ticket");
     } else {
       res.status(200).json(doc);
     }
